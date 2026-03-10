00b1a6959193b21d86d2025824ebe568ffc70fe2
VanDuc <22130050@st.hcmuaf.edu.vn>
2026-03-10 20:21:52 +0700

    create project

---
+++ src/main/java/org/example/donatebackend/DonateBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.donatebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonateBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DonateBackendApplication.class, args);
+    }
+
+}
